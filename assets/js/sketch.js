14f3fe99bc560f19ef427c0cff2d2ed8810bb25f
Maxim Lykov <frise.on@gmail.com>
2020-04-26 14:49:54 +0300

    comments

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,31 +1,47 @@
 const mainContent = document.querySelector('.main__content');
 const mainContentParams = mainContent.getBoundingClientRect();
 
-const randomParams = (width, height, delta, sizeMin, sizeMax) => {
+/**
+ * Функция для управления параметрами "шарика"
+ * 
+ * @param {Number} canvasWidth - ширина холста
+ * @param {Number} canvasHeight - высота холста
+ * @param {Number} delta - шаг для изменения координаты (максимальное смещение)
+ * @param {Number} sizeMin - минимальный размер круга
+ * @param {Number} sizeMax - максимальный размер круга
+ */
+const randomParams = (canvasWidth, canvasHeight, delta, sizeMin, sizeMax) => {
+    // задаём изначальные параметры
     let currentParams = {
-        x: width / 2,
-        y: height / 2,
-        size: sizeMin,
-        strokeColor: 0,
-        fillColor: 255,
-        sizeDelata: 2
+        x: canvasWidth / 2, // круг изначально отображается в центре холста
+        y: canvasHeight / 2, // круг изначально отображается в центре холста
+        size: sizeMin, // у него минимальный размер
+        strokeColor: 0, // цвет обводки черный
+        fillColor: 255, // сам круг белый
+        sizeDelata: 2 // шаг для изменения размера круга
     };
 
+    // направление изменений параметров
+    // -1 - значение будет уменьшаться
+    // 1 - значение будет увеличиваться
     const direction = {
         x: 1,
         y: 1,
-        size: 1
+        size: 1 // изначальное шарик будет увеличиваться
     }
 
     // изменение направления
     setInterval(() => {
-        direction.x = direction.x * (Math.random() > .5 ? 1 : -1);
+        // каждые 5 секунд случайно может поменяться или не поменяться направление по координате X
+        direction.x = Math.random() > .5 ? 1 : -1;
     }, 5000);
     setInterval(() => {
-        direction.y = direction.y * (Math.random() > .5 ? 1 : -1);
+        // каждые 5 секунд случайно может поменяться или не поменяться направление по координате Y
+        direction.y = Math.random() > .5 ? 1 : -1;
     }, 3000);
 
     // смена цвета
+    // каждые 3 секунды меняется на обратный цвет круга и его обводки
     setInterval(() => {
         const temp = currentParams.fillColor;
 
@@ -38,6 +54,7 @@ const randomParams = (width, height, delta, sizeMin, sizeMax) => {
         currentParams.sizeDelata = 40;
 
         setTimeout(() => {
+            // возвращаем к обычному значению sizeDelata
             currentParams.sizeDelata = 2;
         }, 500);
     }, 20000);
@@ -54,6 +71,7 @@ const randomParams = (width, height, delta, sizeMin, sizeMax) => {
         }, 1000);
     }, 27000);
 
+    // возвращаем функцию (getParams в данном случае), вызов которой будет возвращать новые параметры для отрисовки
     return () => {
         // достигли края справа или слева – меняем направление
         if (currentParams.x + delta * direction.x > width || currentParams.x + delta * direction.x < 0) {
@@ -75,15 +93,19 @@ const randomParams = (width, height, delta, sizeMin, sizeMax) => {
 
         // новые параметры
         const newParams = {
-            ...currentParams,
+            fillColor: currentParams.fillColor,
+            strokeColor: currentParams.strokeColor,
+            sizeDelata: currentParams.sizeDelata,
+            // random берет случайное значение из диапазона от 0 до delta – получается неровное смещение/отрисовка рисунка
             x: currentParams.x + random(0, delta) * direction.x,
             y: currentParams.y + random(0, delta) * direction.y,
-            size: currentParams.size + currentParams.sizeDelata * direction.size
+            size: currentParams.size + currentParams.sizeDelata * direction.size,
         };
 
         // старые параметры заменяем новыми
         currentParams = newParams;
 
+        // возвращаем новые параметры для новой отрисовки
         return newParams;
     };
 }
@@ -96,9 +118,11 @@ function setup() {
 
     myCanvas.parent('canvas');
 
+    // задаем FPS, отрисовываться у нас будет 12 кадров в секунду
     frameRate(12);
 }
 
+// draw – это пофакту 1 кадр
 function draw() {
     // получаем параметры
     const params = getParams();
